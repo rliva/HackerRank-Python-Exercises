92d51f5d5aa46e72c440959b7c7bf60025e1bdc1
Ray <livaskaniray@gmail.com>
2024-08-08 19:36:50 -0700

    Create print_function.py

---
+++ Introduction/print_function.py
@@ -0,0 +1,7 @@
+# print all numbers up to and including the given value without spaces and without use of strings
+if __name__ == '__main__':
+    n = int(input())
+    
+for number in range(1, n+1):
+    print(number, end="")
+    
